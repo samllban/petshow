9957ac20dbae863bbf4503a3649a3821f6260f00
samllban <137834390+samllban@users.noreply.github.com>
2024-08-20 21:16:48 -0300

    update project

---
+++ src/components/FeaturedProducts/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FeaturedProducts:React.FC =  () => {
+
+    return (
+        <div className="featured-products"></div>
+    )
+}
+
+export default FeaturedProducts;
