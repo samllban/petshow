0ceed95cd1f7f242fa6603c40cb010946079f601
samllban <137834390+samllban@users.noreply.github.com>
2024-08-23 10:35:23 -0300

    last commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
     return (
         <header className="flex items-center justify-center gap-12 bg-purple-700 p-5">
             <Link to="/" className="text-white no-underline">PetShow</Link>
-            <Link to="/registrationForm" className="text-white no-underline">Formulario</Link>
+            <Link to="/registrationForm" className="text-white no-underline">Form</Link>
         </header>
     );
 }
